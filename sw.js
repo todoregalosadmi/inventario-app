14f6e25eb3ea7da03671e6301c56f9c17d8b4c8a
todoregalosadmi <todoregalosadmi@gmail.com>
2026-04-29 20:32:49 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'inventario-v1';
+const ASSETS = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
